7b4be85780afefa282b9c62336b2bd2d2878ac9a
VintaChlenix <55204918+VintaChlenix@users.noreply.github.com>
2021-07-02 13:01:13 +0300

    Update server.go

--- server.go
+++ server.go
@@ -51,67 +51,23 @@ func (s *server) newClient(conn net.Conn) {
 }
 
 func (s *server) nick(c *client, args []string) {
-	if len(args) < 2 {
-		c.msg("Nick is required. usage: /nick NAME")
-		return
-	}
 
-	c.nick = args[1]
-	c.msg(fmt.Sprintf("OK, now you're %s", c.nick))
 }
 
 func (s *server) join(c *client, args []string) {
-	if len(args) < 2 {
-		c.msg("Room name is required. usage: /join ROOM_NAME")
-		return
-	}
 
-	roomName := args[1]
-
-	r, ok := s.rooms[roomName]
-	if !ok {
-		r = &room{
-			name:    roomName,
-			members: make(map[net.Addr]*client),
-		}
-		s.rooms[roomName] = r
-	}
-	r.members[c.conn.RemoteAddr()] = c
-
-	s.quitCurrentRoom(c)
-	c.room = r
-
-	r.broadcast(c, fmt.Sprintf("%s joined the room", c.nick))
-
-	c.msg(fmt.Sprintf("welcome to %s", roomName))
 }
 
 func (s *server) listRooms(c *client) {
-	var rooms []string
-	for name := range s.rooms {
-		rooms = append(rooms, name)
-	}
 
-	c.msg(fmt.Sprintf("available rooms: %s", strings.Join(rooms, ", ")))
 }
 
 func (s *server) msg(c *client, args []string) {
-	if len(args) < 2 {
-		c.msg("message is required, usage: /msg MSG")
-		return
-	}
 
-	msg := strings.Join(args[1:], " ")
-	c.room.broadcast(c, c.nick+": "+msg)
 }
 
 func (s *server) quit(c *client) {
-	log.Printf("client has left the chat: %s", c.conn.RemoteAddr().String())
 
-	s.quitCurrentRoom(c)
-
-	c.msg("sad to see you go =(")
-	c.conn.Close()
 }
 
 func (s *server) help(c *client) {
@@ -119,9 +75,5 @@ func (s *server) help(c *client) {
 }
 
 func (s *server) quitCurrentRoom(c *client) {
-	if c.room != nil {
-		oldRoom := s.rooms[c.room.name]
-		delete(s.rooms[c.room.name].members, c.conn.RemoteAddr())
-		oldRoom.broadcast(c, fmt.Sprintf("%s has left the room", c.nick))
-	}
+
 }
